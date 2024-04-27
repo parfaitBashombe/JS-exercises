dd631cd44478cb3882669bf1eed75adde102eac4
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-04-27 15:43:55 +0300

    modif1

--- index.js
+++ index.js
@@ -10,4 +10,10 @@ function clearDisplay() {
   display.value = "";
 }
 
-function calculate() {}
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
