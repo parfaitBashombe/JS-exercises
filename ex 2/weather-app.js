25fef5564d954767925634e2de0f6d34fc17ebb0
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-05-20 14:50:52 +0300

    modif 1

--- ex 2/weather-app.js
+++ ex 2/weather-app.js
@@ -1,21 +1,34 @@
 const apiKey = "327e34bab9cec13e2d844a8c8f0677f4";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
 
-  console.log(data);
-
   document.querySelector(".city").innerHTML = data.name;
-
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-
   document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "images/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
+
+  document.querySelector(".weather").style.display = "block";
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
