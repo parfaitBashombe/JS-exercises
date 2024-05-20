ba4df0c649ea63c61be17a76b3f6f8a1cbbc977a
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-05-20 14:13:35 +0300

    modif 1

--- ex 2/weather-app.js
+++ ex 2/weather-app.js
@@ -1,3 +1,21 @@
 const apiKey = "327e34bab9cec13e2d844a8c8f0677f4";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=lisbon";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+checkWeather();
