60c45858f6762f7ac136eba85ea1bd5444321d37
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-05-21 11:47:06 +0300

    modif 1

---
+++ ex 3/ex-3.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i + " * 2 is " + i * 2);
+}
+
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+
+console.log("the sum from 1 to 10 is " + sum);
