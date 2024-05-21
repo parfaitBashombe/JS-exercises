c6e3d876a77bf8a4d5a4a3206dedc9109d7abc0c
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-05-21 11:59:28 +0300

    modif 1

--- ex 3/ex-3.js
+++ ex 3/ex-3.js
@@ -8,3 +8,18 @@ for (let i = 1; i <= 10; i++) {
 }
 
 console.log("the sum from 1 to 10 is " + sum);
+
+for (let i = 1; i <= 10; i++) {
+  if (i < 10) {
+    console.log("");
+  } else {
+    console.log("the sum from 1 to 10 is " + (i * (i + 1)) / 2);
+  }
+}
+
+function summ(i) {
+  let sum = 0;
+  sum = (i * (i + 1)) / 2;
+  console.log("the sum of all the numbers from 0 to " + i + " is " + sum);
+}
+summ(2);
