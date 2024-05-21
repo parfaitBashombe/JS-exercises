18be9464ec5c83c02900b656d4319110f519af0e
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-05-21 17:26:11 +0300

    modif 1

--- ex 3/ex-3.js
+++ ex 3/ex-3.js
@@ -9,21 +9,16 @@ for (let i = 1; i <= 10; i++) {
 
 console.log("the sum from 1 to 10 is " + sum);
 
-for (let i = 1; i <= 10; i++) {
-  if (i < 10) {
-    console.log();
-  } else {
-    console.log("the sum from 1 to 10 is " + (i * (i + 1)) / 2);
-  }
-}
-
 const calculate = document.getElementById("calculate");
 calculate.addEventListener("click", calculateSum);
 
 function calculateSum() {
   let input = document.getElementById("search").value;
   let sum = 0;
-  sum = (input * (input + 1)) / 2;
+
+  for (let i = 0; i <= input; i++) {
+    sum += i;
+  }
   document.getElementById("result").innerHTML =
     "the sum of all the numbers from 0 to " + input + " is " + sum;
 }
