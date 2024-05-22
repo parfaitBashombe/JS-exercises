1e32c84abfd9607f93beaa9505df19d182e3882f
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-05-22 10:26:19 +0300

    modif 1

--- ex4/ex-4.js
+++ ex4/ex-4.js
@@ -27,3 +27,12 @@ function getAnswer(val) {
   return answer;
 }
 console.log(getAnswer(3));
+
+function abtest(a, b) {
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+console.log(abtest(2, 2));
