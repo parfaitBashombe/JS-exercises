6318dbe21e1b7a7d2d180eddca0eea5c0950cd75
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-05-29 20:24:15 +0300

    modif 2

--- ex 7/ex-7.js
+++ ex 7/ex-7.js
@@ -27,3 +27,14 @@ function foo(bar) {
 foo(add);
 
 foo(bar);
+
+//pyramid
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = "";
