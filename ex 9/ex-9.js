f74c6afe80c7a63bba084b1ffd9c338874e1b046
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-06-12 13:05:20 +0300

    modif 1

---
+++ ex 9/ex-9.js
@@ -0,0 +1,9 @@
+const numbers = [3, 2, 1];
+const newFirstNumber = 4;
+// => [ 4, 3, 2, 1 ]
+[newFirstNumber].concat(numbers);
+console.log(newFirstNumber);
+
+// => [ 3, 2, 1, 4 ]
+numbers.concat(newFirstNumber);
+console.log(numbers);
