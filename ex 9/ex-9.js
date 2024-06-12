65ade731c9d68b114115d703d147cfe9c9cdbacc
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-06-12 18:31:48 +0300

    modif 1

--- ex 9/ex-9.js
+++ ex 9/ex-9.js
@@ -1,9 +1,8 @@
 const numbers = [3, 2, 1];
-const newFirstNumber = 4;
+const newFirstNumber = [6, 5, 4];
 // => [ 4, 3, 2, 1 ]
-[newFirstNumber].concat(numbers);
-console.log(numbers);
-
+const veryNew = newFirstNumber.concat(numbers);
+console.log(veryNew);
 // => [ 3, 2, 1, 4 ]
-numbers.concat(newFirstNumber);
-console.log(numbers);
+const modified = numbers.concat(newFirstNumber);
+console.log(modified);
