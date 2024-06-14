8b19684ef0489ece75406087b68b63c5aa432270
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-06-14 20:54:38 +0300

    modif 3

--- ex 9/ex-9.js
+++ ex 9/ex-9.js
@@ -2,7 +2,21 @@ const numbers = [3, 2, 1];
 const newFirstNumber = [6, 5, 4];
 // => [ 4, 3, 2, 1 ]
 const veryNew = newFirstNumber.concat(numbers);
-console.log(veryNew);
+console.log({ newArray: veryNew });
+
+const obj = {
+  x: 4,
+  y: numbers,
+};
+
+console.log(obj.y[2]);
+
+const students = [
+  { name: "Alliance", score: 80, grade: "A", address: ["Q. A", "Q. B"] },
+  { name: "Alliance", score: 80, grade: "A" },
+  { name: "Alliance", score: 80, grade: "A" },
+];
+
 // => [ 3, 2, 1, 4 ]
 const modified = numbers.concat(newFirstNumber);
 console.log(modified);
@@ -12,6 +26,12 @@ let i = 0;
 while (i < 5) {
   console.log(i);
   i++;
+}
+
+console.log("For loop");
+
+for (let i = 0; i < 5; i = i + 2) {
+  console.log(i);
 }
 
 // Reverse loop
@@ -69,14 +89,14 @@ for (i = 0; i < 10; i++) {
 // nested
 for (let i = 0; i < 2; i += 1) {
   for (let j = 0; j < 3; j += 1) {
-    console.log(`${i}-${j}`);
+    console.log({ nested: `${i}-${j}` });
   }
 }
 
 // for...in loop
 
 for (let index in fruits) {
-  console.log(index);
+  console.log({ name: fruits[index] });
 }
 // => 0
 // => 1
@@ -84,7 +104,7 @@ for (let index in fruits) {
 
 // for..of loop
 for (let fruit of fruits) {
-  console.log(fruit);
+  console.log({ fruit });
 }
 
 // functions assigned to variables
@@ -116,3 +136,18 @@ const sum = numberss.reduce((accumulator, curVal) => {
 });
 
 console.log(sum);
+
+const scores = [1, 2, 3, 4, 5];
+
+let sumScore = 0;
+
+for (let i = 0; i < scores.length; i++) {
+  sumScore += scores[i];
+}
+console.log({ sumScore });
+
+const sumReduce = scores.reduce((sum, current) => {
+  return sum + current;
+});
+
+console.log({ sumReduce });
