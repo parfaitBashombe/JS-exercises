46323aca5cd320d2713d48af7711d2f34f0df6cd
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-06-17 12:31:31 +0300

    modif 1

--- ex 10 fibo/ex_10.js
+++ ex 10 fibo/ex_10.js
@@ -10,4 +10,25 @@ function fibonacci(n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
   }
 }
-fib10 = [fibonacci(0)];
+fib10 = [
+  fibonacci(0),
+  fibonacci(1),
+  fibonacci(2),
+  fibonacci(3),
+  fibonacci(4),
+  fibonacci(5),
+  fibonacci(6),
+  fibonacci(7),
+  fibonacci(8),
+  fibonacci(9),
+  fibonacci(10),
+];
+console.log({ fibonacci: fib10 });
+
+//find name
+
+let name = ["John", "Jules", "Elvis"];
+
+for (let i = 0; i < name.length; i++) {
+  result = name[i];
+}
