20270607e8c0fa0e0c56025cfd512034f9b7119a
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-06-20 14:24:46 +0300

    modif 1

--- ex 11/ex_11.js
+++ ex 11/ex_11.js
@@ -1,9 +1,12 @@
 let x = -100;
 console.log((x >>>= 5));
 
-function op() {
-  this.z = 4 + 1;
-  this.y = 6 + 9;
-  return this;
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
 }
-console.log(op());
+const circle = new Circle(2);
+
+console.log(circle);
