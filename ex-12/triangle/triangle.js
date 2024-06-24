9792af00e720f3c7fc0b66bdc50b4322df79081b
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-06-24 12:51:03 +0300

    modif 1

--- ex-12/triangle/triangle.js
+++ ex-12/triangle/triangle.js
@@ -1,5 +1,10 @@
 const char = "*";
 
+for (let j = 6; j > 0; j--) {
+  let print = char.repeat(j);
+  console.log(print);
+}
+
 for (let i = 1; i < 7; i++) {
   let print = char.repeat(i);
   console.log(print);
