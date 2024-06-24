82faf40969ab0bddbda79c586ed76e9d4f81b2b7
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-06-24 16:03:57 +0300

    modif 1

--- ex-12/triangle/triangle.js
+++ ex-12/triangle/triangle.js
@@ -2,7 +2,7 @@ const char = "*";
 const space = " ";
 
 for (let i = 1; i < 7; i++) {
-  let j = 7;
+  let j = 6;
   let printSpace = space.repeat(j - i);
   let print = char.repeat(i);
   console.log(printSpace + print);
@@ -19,3 +19,36 @@ for (i = 0; i <= 5; i++) {
     document.write("*");
   }
 }
+
+//generate pyramid
+function generatePyramid() {
+  var totalNumberofRows = 5;
+  var output = "";
+  for (var i = 1; i <= totalNumberofRows; i++) {
+    for (var j = 1; j <= i; j++) {
+      output += j + "  ";
+    }
+    console.log(output);
+    output = "";
+  }
+}
+
+generatePyramid();
+
+//Pyramid
+function printPyramid(rows) {
+  let pyramid = "";
+  // Defined a for loop to iteracte through each row
+  for (let i = 0; i < rows; i++) {
+    // Add spaces before the first '*' to center the pyramid
+    pyramid += " ".repeat(rows - i - 1);
+    // Add '*' for the current row
+    pyramid += "*".repeat(2 * i + 1);
+    // Move to the next line
+    pyramid += "\n";
+  }
+  console.log(pyramid);
+}
+
+// Example usage: Print a pyramid with 5 rows
+printPyramid(5);
