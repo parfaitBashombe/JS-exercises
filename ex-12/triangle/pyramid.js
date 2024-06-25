9b2e36c53a0af6125d9ce5d4743ec6010eab89e2
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-06-25 15:50:51 +0300

    modif 2

--- ex-12/triangle/pyramid.js
+++ ex-12/triangle/pyramid.js
@@ -3,13 +3,22 @@ space = "";
 let jRows = 6;
 
 for (let i = 0; i < jRows; i++) {
-  space += " ".repeat(jRows - i - 1);
+  space += " ".repeat(jRows - i);
 
   space += char.repeat(2 * i + 1);
 
   space += "\n";
 
   console.log(space);
+}
+//the pyramid grows in the console
+for (let i = 1; i <= jRows; i++) {
+  console.log(" ".repeat(jRows - i) + char.repeat(2 * i - 1));
+}
+
+// generate the steem
+for (let i = 0; i < 4; i++) {
+  console.log(" ".repeat(jRows - 3) + char.repeat(2 * 1 + 1));
 }
 
 //generate pyramid
