0bcaff2ab85c3d3fe0cf7f065d9a9b62a3beb38c
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-06-26 17:43:25 +0300

    modif 1

---
+++ ex-13 challenges/challenges.js
@@ -0,0 +1,11 @@
+// print even numbers from 0-10
+function printEvenNumbers(number) {
+  for (let i = 0; i <= 10; i++) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
+}
+printEvenNumbers(10);
+
+//
